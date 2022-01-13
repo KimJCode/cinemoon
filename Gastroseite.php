58535d8888e54faa999a45f8652013ebf7abb3f7
JuliusStei <julius180602steinbach@gmail.com>
2022-01-13 17:58:49 +0100

    Einrücken und Kommentare

--- Gastroseite.php
+++ Gastroseite.php
@@ -25,13 +25,12 @@
                 </h1>
                 <section>
                     <h2> Snacks</h2><br>
+
                     <h3> Popcorn </h3>
                     <div class = "highlight">
                         <div class = "box" >
-                            
                             <img src = "Medien/Bilder/Popcorn.png" alt = "Popcorn">
                             <!-- https://pluspng.com/png-27884.html -->
-
                         </div>  
                         <p>
                             süß / salzig <br>
@@ -41,14 +40,11 @@
                         </p>  
                     </div>
                     
-                
                     <h3> Nachos </h3>
                     <div class = "highlight" >
                             <div class = "box">
-                                
                                 <img src = "Medien/Bilder/Nachos.png" alt = "Nachos">
                                 <!-- hhttps://webstockreview.net/pict/getfirst -->
-                                
                             </div>  
                             <p>
                                 3 Saucen <br>
@@ -58,8 +54,10 @@
                             </p>
                         </div>  
                 </section>
+
                 <section>
                     <h2> Drinks</h2><br>
+
                     <h3> Softdrinks </h3>
                     <div class = "highlight">
                         <div class = "box">
@@ -70,17 +68,14 @@
                             klein: (0,2 l) 3,5 &#8364; <br>
                             mittel: (0,4 l) 6 &#8364; <br>
                             Groß:  (0,6 l) 8 &#8364;
-
                         </p>
                     </div>  
+                    
                     <h3> Biere </h3>
                     <div class = "highlight" >
                         <div class = "box">
-                            
                             <img src = "Medien/Bilder/Beer6.png" alt = "Bier">
                             <!-- https://www.hiclipart.com/free-transparent-background-png-clipart-phctt/download -->
-                            
-                        
                         </div>  
                         <p>
                             Klein: 4 &#8364; <br>
